c66ead86ee09d9e615f15b43f1b0882129e9f058
Joshua Lai <joshualai@MacBook-Pro-100.local>
2024-12-12 02:00:48 -0800

    Monsters sound effects added

--- dungeon_dept/scripts/monsters/slime.gd
+++ dungeon_dept/scripts/monsters/slime.gd
@@ -1,10 +1,16 @@
 class_name Slime
 extends Monster
 
+@onready var slime_sfx: AudioStreamPlayer = AudioStreamPlayer.new()
+
 func _init() -> void:
 	cost = 10
 	type = MonsterFactory.MonsterType.SLIME
 	
-
+func _ready() -> void:
+	add_child(slime_sfx)
+	slime_sfx.stream = preload("res://sounds/SFX/slime_sfx.wav")
+	slime_sfx.play()
+	
 func _process(_delta: float) -> void:
 	pass
