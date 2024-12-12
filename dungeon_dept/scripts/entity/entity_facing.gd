f21e33bfd811cd7262d8e94194444a5691c661c9
ihhoang <156142939+ihhoang@users.noreply.github.com>
2024-12-12 11:54:36 -0800

    Golem Instance

--- dungeon_dept/scripts/entity/entity_facing.gd
+++ dungeon_dept/scripts/entity/entity_facing.gd
@@ -3,10 +3,15 @@ extends Node2D
 @export var unflipped_position:Vector2
 @export var flipped_position:Vector2
 
-
-func _on_skeleton_character_direction_change(facing: Entity.Facing) -> void:
+func flip(facing: Entity.Facing) -> void:
 	if Entity.Facing.LEFT == facing:
 		position = flipped_position
 	elif Entity.Facing.RIGHT == facing:
 		position = unflipped_position
-	#print("sprite pos: ", position)
+
+func _on_skeleton_character_direction_change(facing: Entity.Facing) -> void:
+	flip(facing)
+
+
+func _on_golem_character_direction_change(facing: Entity.Facing) -> void:
+	flip(facing)
