4563b0fe5df0ca326a5f5a1ef0e8b82123784dd3
Joshua Lai <joshualai@MacBook-Pro-100.local>
2024-12-12 01:11:00 -0800

    Main soundtrack

--- dungeon_dept/scripts/global/soundtrack.gd
+++ dungeon_dept/scripts/global/soundtrack.gd
@@ -3,8 +3,7 @@ extends Node
 @onready var menu_soundtrack: AudioStreamPlayer = AudioStreamPlayer.new()
 @onready var battle_soundtrack: AudioStreamPlayer = AudioStreamPlayer.new()
 
-func play_menu_music() -> void:
-	stop_battle_music()
+func _ready() -> void:
 	add_child(menu_soundtrack)
 	menu_soundtrack.stream = preload("res://sounds/Music/Goblins_Den_(Regular).ogg")   
 	menu_soundtrack.play()
@@ -18,6 +17,10 @@ func play_battle_music() -> void:
 func stop_menu_music() -> void:
 	if menu_soundtrack.playing:
 		menu_soundtrack.stop()
+
+func play_menu_music() -> void:
+	if not menu_soundtrack.playing:
+		menu_soundtrack.play()
 		
 func stop_battle_music() -> void:
 	if battle_soundtrack.playing:
