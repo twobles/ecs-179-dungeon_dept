096d85f0955e6da685da5d8d43164bb9c0e04d9f
ihhoang <156142939+ihhoang@users.noreply.github.com>
2024-12-12 10:59:51 -0800

    REPO_TRANSFER

--- dungeon_dept/scripts/monsters/monster.gd
+++ dungeon_dept/scripts/monsters/monster.gd
@@ -1,5 +1,13 @@
 class_name Monster
 extends Entity
 
-var cost: int = 20
-var type: MonsterFactory.MonsterType = MonsterFactory.MonsterType.SKELETON
+# cost to deploy
+@export var cost: float = 0.0
+
+func _ready() -> void:
+	cost = 100.0
+	#movement_speed = 4000.0
+	team = 1
+	facing = Facing.LEFT
+	super()
+	
--- dungeon_dept/scripts/monsters/skeleton.gd
+++ dungeon_dept/scripts/monsters/skeleton.gd
@@ -1,19 +1,6 @@
 class_name Skeleton
 extends Monster
 
-@onready var skeleton_sfx: AudioStreamPlayer = AudioStreamPlayer.new()
-
-
-func _init() -> void:
-	pass
-	
-	
 func _ready() -> void:
-	add_child(skeleton_sfx)
-	skeleton_sfx.stream = preload("res://sounds/SFX/skeleton_sfx.wav")
-	skeleton_sfx.play()
-
-#
-func _process(_delta: float) -> void:
-	pass
-	
+	team = 1
+	melee_range = 40
